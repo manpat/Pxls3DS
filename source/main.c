f3b4c30439ec69d734adaf69f40c2b3e4ccdac16
manpat <pat@patsown.com>
2017-04-12 16:10:52 +0100

    Converted to tabs (for consistency); Fixed some typos; fread now reads more than sizeof(void*) bytes

--- source/main.c
+++ source/main.c
@@ -13,66 +13,68 @@
 
 char pxlsEndpoint[] = "http://pxls.space"; // Pxls endpoint
 char* pxlsApi[1] = {
-  "/boarddata"
+	"/boarddata"
 }; // Api endpoints
 
 int boardInfo[3] = {
-  1999,
-  1999,
-  2000
+	1999,
+	1999,
+	2000
 }; // X, Y, Width of board
 char *boardColors[16] = {
-  "FFFFFF",
-  "E4E4E4",
-  "888888",
-  "222222",
-  "FFA7D1",
-  "E50000",
-  "E59500",
-  "A06A42",
-  "E5D900",
-  "94E044",
-  "02BE01",
-  "00D3DD",
-  "0083C7",
-  "0000EA",
-  "CF6EE4",
-  "820080"
+	"FFFFFF",
+	"E4E4E4",
+	"888888",
+	"222222",
+	"FFA7D1",
+	"E50000",
+	"E59500",
+	"A06A42",
+	"E5D900",
+	"94E044",
+	"02BE01",
+	"00D3DD",
+	"0083C7",
+	"0000EA",
+	"CF6EE4",
+	"820080"
 }; // Board colors (in hex)
 
 //boardData.board[boardInfo[1]+boardInfo[2]*boardInfo[3]] ?
 
 struct boardData {
-  unsigned char *board;
-} boardData;
+	unsigned char *board;
+};
+
+const unsigned int BOARD_SIZE = 4u<<20;
 
 int drawBoard()
 {
-  struct boardData board;
-  board.board = linearAlloc(4u<<20);
+	struct boardData board;
+	board.board = linearAlloc(BOARD_SIZE);
 
-  FILE *fpr = fopen("boarddata", "r");
-  if(fpr != NULL)
-  {
-    fread(board.board, 1, sizeof(board), fpr);
-    //printf("%d\n", boardColors[board.board[0+0*2000]]);
-    //fclose(fpr);
-  }
+	FILE *fpr = fopen("boarddata", "r");
+	if(fpr != NULL)
+	{
+		fread(board.board, 1, BOARD_SIZE, fpr);
+		//printf("%d\n", boardColors[board.board[0+0*2000]]);
+		// fclose(fpr);
+	}
 
-  int x;
-  int y;
+	int x;
+	int y;
 
-  int r, g, b;
+	int r, g, b;
 
-  for (x = 0; x < 1999; x++) {
-    for (y = 0; y < 1999; x++) {
-      sscanf(boardColors[board.board[x+y*2000]], "%02x%02x%02x", &r, &g, &b);
-      sf2d_draw_rectangle(x, y, 1, 1, RGBA8(0x00, r, g, b));
-    }
-  }
+	for (x = 0; x < 2000; x++) {
+		for (y = 0; y < 2000; y++) {
+			sscanf(boardColors[board.board[x+y*2000]], "%02x%02x%02x", &r, &g, &b);
+			sf2d_draw_rectangle(x, y, 1, 1, RGBA8(0x00, r, g, b));
+		}
+	}
 
-  fclose(fpr);
-  return 0;
+	fclose(fpr);
+	return 0;
 }
 
 int main()
@@ -83,11 +85,11 @@ int main()
 	sf2d_set_clear_color(RGBA8(0xFF, 0xFF, 0xFF, 0xFF));
 	sf2d_set_3D(0);
 
-  while (aptMainLoop())
-  {
+	while (aptMainLoop())
+	{
 		sf2d_start_frame(GFX_TOP, GFX_LEFT);
 			//sf2d_draw_rectangle(5, 5, 1, 1, RGBA8(0x00, 0xFF, 0xFF, 0xFF));
-      drawBoard();
+			drawBoard();
 		sf2d_end_frame();
 
 		sf2d_swapbuffers();
