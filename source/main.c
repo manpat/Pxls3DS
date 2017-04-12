6867d80eeae5b6208e644312d29c7c65756cec6c
manpat <pat@patsown.com>
2017-04-12 16:22:00 +0100

    Moved board init out of drawBoard

--- source/main.c
+++ source/main.c
@@ -48,32 +48,36 @@ struct boardData {
 
 const unsigned int BOARD_SIZE = 4u<<20;
 
-int drawBoard()
+int initBoard(struct boardData* board)
 {
-	struct boardData board;
-	board.board = linearAlloc(BOARD_SIZE);
+	board->board = linearAlloc(BOARD_SIZE);
 
 	FILE *fpr = fopen("boarddata", "r");
-	if(fpr != NULL)
-	{
-		fread(board.board, 1, BOARD_SIZE, fpr);
-		//printf("%d\n", boardColors[board.board[0+0*2000]]);
-		// fclose(fpr);
-	}
+	if(fpr == NULL)
+		return 1;
 
+	fread(board->board, 1, BOARD_SIZE, fpr);
+	fclose(fpr);
+
+	return 0
+}
+
+int drawBoard(struct boardData* board)
+{
 	int x;
 	int y;
 
 	int r, g, b;
 
-	for (x = 0; x < 2000; x++) {
-		for (y = 0; y < 2000; y++) {
-			sscanf(boardColors[board.board[x+y*2000]], "%02x%02x%02x", &r, &g, &b);
+	const int BOARD_WIDTH = 2000;
+
+	for (y = 0; y < 2000; y++) {
+		for (x = 0; x < BOARD_WIDTH; x++) {
+			sscanf(boardColors[board->board[x+y*BOARD_WIDTH]], "%02x%02x%02x", &r, &g, &b);
 			sf2d_draw_rectangle(x, y, 1, 1, RGBA8(0x00, r, g, b));
 		}
 	}
 
-	fclose(fpr);
 	return 0;
 }
 
@@ -85,11 +89,15 @@ int main()
 	sf2d_set_clear_color(RGBA8(0xFF, 0xFF, 0xFF, 0xFF));
 	sf2d_set_3D(0);
 
+	struct boardData board;
+	if(initBoard(&board))
+		return 1;
+
 	while (aptMainLoop())
 	{
 		sf2d_start_frame(GFX_TOP, GFX_LEFT);
 			//sf2d_draw_rectangle(5, 5, 1, 1, RGBA8(0x00, 0xFF, 0xFF, 0xFF));
-			drawBoard();
+			drawBoard(&board);
 		sf2d_end_frame();
 
 		sf2d_swapbuffers();
